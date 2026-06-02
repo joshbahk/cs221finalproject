0a2a614ac4820426ee7b10535f24c37a9fe5d8ae
Gabriella Ugwonali <gabriella.michele@icloud.com>
2026-06-02 13:49:44 -0700

    fixed imports and root path
    working start for backend

--- web/backend/server.py
+++ web/backend/server.py
@@ -1,3 +1,11 @@
+import sys
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parents[2]
+SRC = ROOT / "src"
+
+sys.path.insert(0, str(SRC))
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
